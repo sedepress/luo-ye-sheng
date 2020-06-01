d92163faac2880764a2f421ed655852e20e38001
zs <zhengsheng930118@163.com>
2020-06-01 16:33:23 +0800

    add

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -10,6 +10,9 @@ class IndexController extends Controller
     public function index(Request $request)
     {
         $app = Factory::officialAccount(config('wechat.official_account.default'));
+        $app->server->push(function ($message) {
+            return "您好！欢迎使用 EasyWeChat!";
+        });
         $response = $app->server->serve();
 
         return $response;
