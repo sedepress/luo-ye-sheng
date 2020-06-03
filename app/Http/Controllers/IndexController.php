987d78d671138d3e44c85c484d8604dc343c9e12
zs <zhengsheng930118@163.com>
2020-06-03 16:51:46 +0800

    fix

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -13,39 +13,13 @@ class IndexController extends Controller
         $app->server->push(function ($message) {
             switch ($message['MsgType']) {
                 case 'event':
-                    return '欢迎来到落叶生
-                            输入以下指令编号执行相关操作
-                            0、重置指令
-                            1、打怪
-                            2、锻造
-                            3、挖矿
-                            4、领取疲劳
-                            5、商店|装备|邀请
-                            官方QQ群：1023380085';
+                    return "欢迎来到落叶生\n输入以下指令编号执行相关操作\n0、重置指令\n1、打怪\n2、锻造\n3、挖矿\n4、领取疲劳\n5、商店|装备|邀请\n官方QQ群：1023380085";
                     break;
                 case 'text':
                     return "欢迎来到落叶生\n输入以下指令编号执行相关操作\n0、重置指令\n1、打怪\n2、锻造\n3、挖矿\n4、领取疲劳\n5、商店|装备|邀请\n官方QQ群：1023380085";
                     break;
-                case 'image':
-                    return '收到图片消息';
-                    break;
-                case 'voice':
-                    return '收到语音消息';
-                    break;
-                case 'video':
-                    return '收到视频消息';
-                    break;
-                case 'location':
-                    return '收到坐标消息';
-                    break;
-                case 'link':
-                    return '收到链接消息';
-                    break;
-                case 'file':
-                    return '收到文件消息';
-                // ... 其它消息
                 default:
-                    return '收到其它消息';
+                    return '亲，只接受字符回复哦！';
                     break;
             }
         });
