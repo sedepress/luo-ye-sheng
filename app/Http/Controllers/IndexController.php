1748d7a8c54b67577e1a2e7a2e37c007365cc61c
zs <zhengsheng930118@163.com>
2020-06-03 15:08:39 +0800

    switch

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,7 +11,33 @@ class IndexController extends Controller
     {
         $app = Factory::officialAccount(config('wechat.official_account.default'));
         $app->server->push(function ($message) {
-            return "您好！欢迎使用 EasyWeChat!";
+            switch ($message['MsgType']) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                case 'file':
+                    return '收到文件消息';
+                default:
+                    return '收到其它消息';
+                    break;
         });
         $response = $app->server->serve();
 
