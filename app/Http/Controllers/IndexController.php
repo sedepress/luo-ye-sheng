076cd476aa6d45429920ff22e8820fadab32dcc6
zs <zhengsheng930118@163.com>
2020-07-02 10:30:22 +0800

    fix

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -37,7 +37,7 @@ class IndexController extends Controller
                     if (Redis::hSetnx($key, 'openid', $message['FromUserName'])) {
                         $this->userService->register($message);
                     }
-                    logger()->error(json_encode($message->Event));
+                    logger()->error(json_encode($message));
 
                     return self::MENU;
                     break;
